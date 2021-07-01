60da0d957bf89ab7939dc36b6b2fb8951046d687
Ivan <fenomen_ny@mail.ru>
2021-07-01 03:00:17 +0300

    first

---
+++ script.js
@@ -0,0 +1,31 @@
+const tabsBtn = document.querySelectorAll('.tabs__nav-btn');
+const tabsItems = document.querySelectorAll('.tabs__item');
+
+tabsBtn.forEach(onTabClick);
+
+function onTabClick(item) {
+  item.addEventListener('click', function () {
+    let currentBtn = item;
+    let tabId = currentBtn.getAttribute('data-tab');
+    let currentTab = document.querySelector(tabId);
+    
+		if(!currentBtn.classList.contains('active')) {
+			tabsBtn.forEach(function (item) {
+				item.classList.remove('active');
+			});
+	
+			tabsItems.forEach(function (item) {
+				item.classList.remove('active');
+			});
+	
+			
+			currentBtn.classList.add('active');
+			currentTab.classList.add('active');
+		}
+
+
+  });
+}
+
+// виртуальный клик по 1му элем
+document.querySelector('.tabs__nav-btn:nth-child(2)').click();
